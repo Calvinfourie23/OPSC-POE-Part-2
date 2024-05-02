23f48504c5f48bced34130245a1c47686974ad09
calvin <calvin.f.9300@gmail.com>
2024-05-03 00:34:32 +0200

    Version 7 UI changes

--- app/src/main/java/com/example/opsc7311poepart2/ItemTimesheetActivity.kt
+++ app/src/main/java/com/example/opsc7311poepart2/ItemTimesheetActivity.kt
@@ -2,8 +2,11 @@ package com.example.opsc7311poepart2
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
+import android.widget.Button
+import android.content.Intent
 class ItemTimesheetActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.item_timesheet)
--- app/src/main/java/com/example/opsc7311poepart2/LogAdapter.kt
+++ app/src/main/java/com/example/opsc7311poepart2/LogAdapter.kt
@@ -4,8 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -29,12 +31,15 @@ class LogAdapter(private val context: Context, private val logsList: List<TimeLo
     inner class LogViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val dateTextView: TextView = itemView.findViewById(R.id.tvDate)
         private val categoryTextView: TextView = itemView.findViewById(R.id.tvTask)
+        private val imageView: ImageView = itemView.findViewById(R.id.ivTaskImage)
 
         fun bind(log: TimeLog) {
             // Convert Unix timestamp to formatted date string
             val formattedDate = formatDate(log.selectedDate.toLong())
             dateTextView.text = formattedDate
             categoryTextView.text = log.selectedCategory
+
+            Picasso.get().load(log.imageUrl).into(imageView)
         }
 
         // Function to format Unix timestamp to "dd MMMM yyyy" format
