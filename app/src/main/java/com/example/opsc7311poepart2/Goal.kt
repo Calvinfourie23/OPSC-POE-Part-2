06016f821fd8639c4b4aeb96ca574aa9487a8a4a
calvin <calvin.f.9300@gmail.com>
2024-05-03 16:06:31 +0200

    Version 8.1

--- app/src/main/java/com/example/opsc7311poepart2/Goal.kt
+++ app/src/main/java/com/example/opsc7311poepart2/Goal.kt
@@ -3,6 +3,6 @@ package com.example.opsc7311poepart2
 data class Goal(
     val id: String? = null,
     val name: String = "",
-    val duration: Int = 0, // Duration in minutes
-    val maxDuration: Int
+    val minDuration: Int = 0, // Minimum duration in minutes
+    val maxDuration: Int = 0 // Maximum duration in minutes
 )
